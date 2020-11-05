c6c6d2936780a2c6b10af4a82550db739e57fe99
Ankit Verma <ankitverma_0810@yahoo.co.in>
2020-11-05 14:59:40 +0530

    make it better

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import loadable from '@loadable/component';
 import { Redirect } from 'react-router';
 
 import App from './App';
 import ProgramsPage from './pages/ProgramsPage';
 import NotFoundPage from './pages/NotFoundPage';
-
-const TestPage = loadable(() => import('./pages/TestPage'));
 
 export default [
     {
@@ -20,10 +17,6 @@ export default [
             {
                 path: '/programs',
                 ...ProgramsPage
-            },
-            {
-                path: '/test',
-                render: (props) => <TestPage {...props} />
             },
             {
                 /* by not mentioning any path, react router will render this page if no route is matched above */
